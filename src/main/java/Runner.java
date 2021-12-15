fcb5d0522f26f9a9b40e3579dc2b1bb5775ca7c7
vbozh <viktoria.bozhko@jetbrains.com>
2021-12-15 12:42:20 +0300

    added line

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -12,6 +12,7 @@ public class Runner {
             new PassengerPlane("Boeing-737-800", 940, 12300, 63870, 192),
             new PassengerPlane("Boeing-747", 980, 16100, 70500, 242),
             new PassengerPlane("Airbus A320", 930, 11800, 65500, 188),
+            new PassengerPlane("Airbus A330", 990, 14800, 80500, 222),
             new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
             new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
             new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
