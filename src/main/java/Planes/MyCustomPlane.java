e822c4ea205566ce2e73487a7ddb7dd1ffdb1bf3
vbozh <viktoria.bozhko@jetbrains.com>
2021-12-15 12:57:44 +0300

    renamed class

---
+++ src/main/java/Planes/MyCustomPlane.java
@@ -0,0 +1,9 @@
+package Planes;
+
+public class MyCustomPlane extends Plane {
+    public MyCustomPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    }
+
+    //TODO implement it later
+}
